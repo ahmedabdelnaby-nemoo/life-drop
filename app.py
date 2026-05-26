ed27acda5c89e36537f4a6ef16880316747727f3
Ahmed abd elnaby <a01145389704@gmail.com>
2026-05-26 17:27:01 +0300

    first upload

---
+++ app.py
@@ -0,0 +1,60 @@
+
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+# Home Page
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+# Show All Donors
+@app.route('/show')
+def show():
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM donors")
+    donors = cursor.fetchall()
+    conn.close()
+    return render_template("show.html", donors=donors)
+
+# Donors Page
+@app.route('/donors')
+def donors():
+    return render_template("donors.html")
+
+# Save Donor
+@app.route('/save', methods=['POST'])
+def save():
+
+    name = request.form['name']
+    blood = request.form['blood']
+    city = request.form['city']
+    phone = request.form['phone']
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS donors(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            blood TEXT,
+            city TEXT,
+            phone TEXT
+        )
+    ''')
+
+    cursor.execute('''
+        INSERT INTO donors(name, blood, city, phone)
+        VALUES (?, ?, ?, ?)
+    ''', (name, blood, city, phone))
+
+    conn.commit()
+    conn.close()
+
+    return render_template("success.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
