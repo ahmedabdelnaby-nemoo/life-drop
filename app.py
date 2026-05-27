2c0b48f76747c9092e79f4dafaedec0d8617ce44
Ahmed abd elnaby <a01145389704@gmail.com>
2026-05-27 21:00:29 +0300

    Add splash page with logo and update index text

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ import os
 
 app = Flask(__name__)
 
-# Home Page
+# Splash Page - Logo
 @app.route('/')
 def home():
+    return render_template("splash.html")
+
+# Main Registration Page
+@app.route('/index')
+def index():
     return render_template("index.html")
 
 # Show All Donors
